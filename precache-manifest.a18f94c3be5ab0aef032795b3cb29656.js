89de3a620a36ace9b359f4e54b091038a89091c4
ruirui <yruirui96@gmail.com>
2021-11-13 14:35:13 +0800

    deploy

---
+++ precache-manifest.a18f94c3be5ab0aef032795b3cb29656.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6c849443d2dcff6e7b",
+    "url": "/account-dist/css/app.05d9a040.css"
+  },
+  {
+    "revision": "d28caa51119c6d4a7c2ff2226e29c410",
+    "url": "/account-dist/index.html"
+  },
+  {
+    "revision": "eeae241dfc6b46bbd104",
+    "url": "/account-dist/js/about.c8b0861a.js"
+  },
+  {
+    "revision": "ee6c849443d2dcff6e7b",
+    "url": "/account-dist/js/app.f222f85d.js"
+  },
+  {
+    "revision": "0be3298e85e782dd681b",
+    "url": "/account-dist/js/chunk-vendors.d0806bc7.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/account-dist/manifest.json"
+  },
+  {
+    "revision": "486da0f388159b192014e029e2ffc4a6",
+    "url": "/account-dist/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-dist/robots.txt"
+  }
+]);
